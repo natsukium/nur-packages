ca2ca857f44e52edb085c052a9c81685707ffaf4
natsukium <tomoya.otabi@gmail.com>
2022-09-21 23:03:15 +0900

    hackgen: add meta

--- pkgs/data/fonts/hackgen/default.nix
+++ pkgs/data/fonts/hackgen/default.nix
@@ -11,4 +11,11 @@ in
       unzip -j $downloadedFile \*.ttf -d $out/share/fonts/hackgen
     '';
     sha256 = "sha256-UL6U/q2u1UUP31lp0tEnFjzkn6dn8AY6hk5hJhPsOAE=";
+
+    meta = with lib; {
+      description = "composite font of Hack and GenJyuu-Gothic";
+      homepage = "https://github.com/yuru7/HackGen";
+      license = licenses.ofl;
+      platforms = platforms.unix;
+    };
   }
