f77545712bf6c598abac4a4a65d70865dd31c1c4
natsukium <tomoya.otabi@gmail.com>
2024-05-01 18:29:51 +0900

    rofi-rbw: update patch

--- pkgs/rofi-rbw/default.nix
+++ pkgs/rofi-rbw/default.nix
@@ -4,9 +4,9 @@ rofi-rbw.overrideAttrs (
     patches = (oldAttrs.patches or [ ]) ++ [
       (fetchpatch {
         name = "fuzzel-support.patch";
-        url = "https://github.com/natsukium/rofi-rbw/commit/20e56d74701a3d640975de7c215d788fbca0e34e.patch";
-        includes = [ "src/rofi_rbw/selector.py" ];
-        hash = "sha256-7HhDPFj7qvblYRzAv3Jt05t+na+7QyDwHwFLbhxZPqc=";
+        url = "https://github.com/natsukium/rofi-rbw/commit/9db101fec3941678f725c9ba7ddc394afef5a089.patch";
+        includes = [ "src/rofi_rbw/selector/fuzzel.py" "src/rofi_rbw/selector/selector.py" ];
+        hash = "sha256-TnKFJDr9PSe1gk1wzYFBNO9xDxKxPyw4LtE9K3e+vk4=";
       })
     ];
 
